bab4db347dbe2084ee761d161905583775df5ec3
Alexandr <hodukovaleksandr@yandex.ru>
2019-03-26 12:49:56 +0300

    refactored DataConfig and added AppConfig

--- src/main/java/com/simpleblog/config/DataConfig.java
+++ src/main/java/com/simpleblog/config/DataConfig.java
@@ -2,71 +2,48 @@ package com.simpleblog.config;
 
 import java.util.Properties;
 
-import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import com.simpleblog.security.UserDetailsServiceImpl;
-import com.simpleblog.service.ArticleService;
-import com.simpleblog.service.UserService;
-import com.simpleblog.utils.EnumUtilsProp;
 
 @Configuration
 @EnableTransactionManagement
 @PropertySource("classpath:db.properties")
+@PropertySource("classpath:token.properties")
 @EnableJpaRepositories("com.simpleblog.repository")
-@ComponentScan("com.simpleblog")
 public class DataConfig {
-    
-    @Bean
-	public ArticleService articleService() {
-		return new ArticleService();
+	
+	@Bean
+	public ConfigPropDatabase confiPropgDb() {
+		return new ConfigPropDatabase();
 	}
-    
-    @Bean UserService userService() {
-    	return new UserService();
-    }
-    
-    @Bean
-    public UserDetailsService getUserDetailsService(){
-        return new UserDetailsServiceImpl();
-    }
-    
-    @Resource
-    private Environment env;
-    
+	
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
-        dataSource.setDriverClassName(env.getRequiredProperty(EnumUtilsProp.DB_DRIVER.getString()));
-        dataSource.setUrl(env.getRequiredProperty(EnumUtilsProp.DB_URL.getString()));
-        dataSource.setUsername(env.getRequiredProperty(EnumUtilsProp.DB_USERNAME.getString()));
-        dataSource.setPassword(env.getRequiredProperty(EnumUtilsProp.DB_PASSWORD.getString()));
+        dataSource.setDriverClassName(confiPropgDb().getDbDriver());
+        dataSource.setUrl(confiPropgDb().getDbUrl());
+        dataSource.setUsername(confiPropgDb().getDbUsername());
+        dataSource.setPassword(confiPropgDb().getDbPassword());
 
         return dataSource;
     }
     
     private Properties getHibernateProperties() {
         Properties properties = new Properties();
-        properties.put(EnumUtilsProp.HIBERNATE_DIALECT.getString(), 
-        		env.getRequiredProperty(EnumUtilsProp.HIBERNATE_DIALECT.getString()));
-        properties.put(EnumUtilsProp.HIBERNATE_SHOW_SQL.getString(), 
-        		env.getRequiredProperty(EnumUtilsProp.HIBERNATE_SHOW_SQL.getString()));
+        properties.put(confiPropgDb().getDialect(), confiPropgDb().getDialect());
+        properties.put(confiPropgDb().getShowSql(), confiPropgDb().getShowSql());
 
         return properties;
     }
@@ -76,7 +53,7 @@ public class DataConfig {
         LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = 
         		new LocalContainerEntityManagerFactoryBean();
         entityManagerFactoryBean.setDataSource(dataSource());
-        entityManagerFactoryBean.setPackagesToScan(env.getRequiredProperty(EnumUtilsProp.PACKAGES_TO_SCAN.getString()));
+        entityManagerFactoryBean.setPackagesToScan(confiPropgDb().getPackageToScan());
         entityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter());
         entityManagerFactoryBean.setJpaProperties(getHibernateProperties());
 
